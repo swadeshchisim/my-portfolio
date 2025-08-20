e593f748a7398bc3ee65df86902d8673044f6287
swadeshchisim <schisim195@gmail.com>
2025-08-20 10:50:40 +0600

    tailwind css cdn added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            brand: '#1e40af',
+          },
+          fontFamily: {
+            custom: ['"Comic Sans MS"', 'cursive']
+          }
+        }
+      }
+    }
